75c34260e1ddc6e5bf2b18725f63b7a2ba210742
MichaelOMeara <124404270+MichaelOMeara@users.noreply.github.com>
2024-11-26 11:39:23 -0500

    Slight game manager tweaks

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    //public TextMeshProUGUI GameOver;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //GameOver.gameObject.SetActive(true);
     }
 
     // Update is called once per frame
