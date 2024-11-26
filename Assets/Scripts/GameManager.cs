1bf907c26cbd4a7d0e355066d548d9d1e1b56ac5
MichaelOMeara <124404270+MichaelOMeara@users.noreply.github.com>
2024-11-26 12:17:03 -0500

    Game over logic reenabled

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,14 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    //public TextMeshProUGUI GameOver;
-    // Start is called before the first frame update
+    public TextMeshProUGUI GameOver;
+    //Start is called before the first frame update
     void Start()
     {
-        //GameOver.gameObject.SetActive(true);
+       GameOver.gameObject.SetActive(true);
     }
 
     // Update is called once per frame
